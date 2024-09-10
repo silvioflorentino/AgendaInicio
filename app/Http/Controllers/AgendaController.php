94e2a1d885ef8263d1ee78b25d0e53db187d6e09
SILVIO WANDERSON FLORENTINO <professorsilvioflorentino@yahoo.com.br>
2024-09-10 17:36:32 -0300

    agenda inicio8

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -8,8 +8,38 @@ use Illuminate\Support\Facades\Redirect;
 class AgendaController extends Controller
 {
     //CRUD
+    //C-create salvar os contatos na tabela
     function SalvarContato(Request $request){
-        Agenda::create($request);
+
+        $registros = $request->validate([
+            'nome'=>'string|required',
+            'email'=>'string|required'
+        ]);
+
+        Agenda::create($registros);
         return Redirect::route('/');
+    }
+    //CRUD
+    //R - Read mostrar os registros da tabela
+    public function ShowFormulario(){
+        return view('cadastroagenda');
+    }
+
+    //CRUD
+    //D - Delete Apagar um registro na tabela
+    public function destroy(Agenda $id){
+        $id->delete();
+        return Redirect::route('home');
+    }
+    //CRUD
+    //U- Update alterar um registro
+    public function update(Agenda $id, Request $request){
+        $registros = $request->validate([
+            'nome'=> 'string|required',
+            'email'=> 'string|required'
+        ]);
+        $id->fill($registros);
+        $id->save();
+        return Redirect::route('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AgendaController;
 
-Route::get('/', function () {
-    return view('cadastroagenda');
-});
+Route::get('/salvar-agenda', [AgendaController::class,'ShowFormulario'])->name('mostra-formulario');
 
-Route::post('/salvar',[AgendaController::class,'SalvarContato']);
+Route::post('/salvar-agenda',[AgendaController::class,'SalvarContato'])->name('cadastrar-formulario');
+
+Route::delete('/apaga-agenda/{id}',[AgendaController::class,'destroy'])->name('apaga-agenda');
+
+Route::put('/altera-agenda/{id}',[AgendaController::class,'update'])->name('altera-agenda');
+
+
